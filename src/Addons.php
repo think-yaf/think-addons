0531ca42d96f8495f0a8823853606c30e1a57b75
tanminghe <thinkyaf@qq.com>
2021-03-16 18:59:28 +0800

    初始化

---
+++ src/Addons.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace think\app;
+
+use Closure;
+use think\App;
+use think\exception\HttpException;
+use think\Request;
+use think\Response;
+
+/**
+ * 多应用模式支持
+ */
+class Addons
+{
+
+    /** @var App */
+    protected $app;
+
+    /**
+     * 应用名称
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * 应用名称
+     * @var string
+     */
+    protected $appName;
+
+    /**
+     * 应用路径
+     * @var string
+     */
+    protected $path;
+
+    public function __construct(App $app)
+    {
+        $this->app  = $app;
+        $this->name = $this->app->http->getName();
+        $this->path = $this->app->http->getPath();
+    }
+
+    /**
+     * 多应用解析
+     * @access public
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function handle($request, Closure $next)
+    {
+        $next($request);
+    }
+}
